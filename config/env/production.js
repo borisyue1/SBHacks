1e5eb7a51c966ee45bc5d1370bab95eb0d672ecd
Boris Yue <borisyue@yahoo.com>
2017-01-24 02:55:54 -0800

    fixed production.js typo

---
+++ config/env/production.js
@@ -0,0 +1,18 @@
+//store application variables
+//either development environment or production environment, which will have different variables
+var port = process.env.PORT || 1337;
+
+module.exports = {
+    port: port,
+    db: 'mongodb://boris:snappit@ds127949.mlab.com:27949/snappit',
+    facebook: {
+        clientID: '270131086738454',
+        clientSecret: 'e3fb58da7052c530e407d4f0c0f345f8',//id and secret generated from facebook developer website
+        callbackURL: 'http://localhost:'+ port +'/oauth/facebook/callback'
+    },
+    twitter: {
+	    clientID: '1iWdQ80rrkMegueawkLvpQ6Ji',
+	    clientSecret: 'Vncm4wDvEKUARywrKAY8MfgIaYENnOMaUWXZa1Mldnbe1O1jcq',
+	    callbackURL: 'http://localhost:1337/oauth/twitter/callback'
+    }
+};
