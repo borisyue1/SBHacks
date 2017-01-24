0126ab4b55e7bb95e115d996931d71de30bea30a
Boris Yue <borisyue@yahoo.com>
2017-01-24 03:14:48 -0800

    changed facebook and twitter production callbacks

--- config/env/production.js
+++ config/env/production.js
@@ -8,11 +8,11 @@ module.exports = {
     facebook: {
         clientID: '270131086738454',
         clientSecret: 'e3fb58da7052c530e407d4f0c0f345f8',//id and secret generated from facebook developer website
-        callbackURL: 'http://localhost:'+ port +'/oauth/facebook/callback'
+        callbackURL: 'https://snappit-borisyue.herokuapp.com/oauth/facebook/callback'
     },
     twitter: {
 	    clientID: '1iWdQ80rrkMegueawkLvpQ6Ji',
 	    clientSecret: 'Vncm4wDvEKUARywrKAY8MfgIaYENnOMaUWXZa1Mldnbe1O1jcq',
-	    callbackURL: 'http://localhost:1337/oauth/twitter/callback'
+	    callbackURL: 'https://snappit-borisyue.herokuapp.com/oauth/twitter/callback'
     }
 };
